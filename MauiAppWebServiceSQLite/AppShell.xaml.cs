ef0ca4b6562cfd0a1e96a558ea4ab8cf45d6b0b8
KauanGomesFernandes <kauangomesfernandespro@gmail.com>
2024-10-17 16:24:14 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppWebServiceSQLite/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppWebServiceSQLite
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
